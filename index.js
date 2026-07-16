2decdefcaf17cde8e538a6ee8dd9b9c23006994c
Ngatchou Christian <ngachouchris2020@gmail.com>
2026-07-16 11:46:51 +0100

    Use Render Firebase environment variables

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
-const serviceAccount = require("./radio-f14ca-firebase-adminsdk-fbsvc-42deb1673c.json");
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 const app = express();
 
