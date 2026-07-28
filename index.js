0756da752eceebe46baa5b145ed57a85ef49a731
Ngatchou Christian <ngachouchris2020@gmail.com>
2026-07-28 10:44:54 +0100

    Utilisation Firebase via variables Render

--- index.js
+++ index.js
@@ -24,21 +24,31 @@ try {
 
   admin.initializeApp({
 
-    credential: admin.credential.cert(
-      require("./radio-f14ca-firebase-adminsdk-fbsvc-42deb1673c.json")
-    )
+    credential: admin.credential.cert({
+
+      projectId:
+        process.env.FIREBASE_PROJECT_ID,
+
+      clientEmail:
+        process.env.FIREBASE_CLIENT_EMAIL,
+
+      privateKey:
+        process.env.FIREBASE_PRIVATE_KEY.replace(
+          /\\n/g,
+          "\n"
+        )
+
+    })
 
   });
 
-
   console.log("Firebase Admin connecté");
-
 
 } catch (error) {
 
   console.error(
     "Erreur connexion Firebase Admin :",
-    error.message
+    error
   );
 
 }
